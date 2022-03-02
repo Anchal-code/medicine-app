1f21f19c58ae4815e710da6b0088a214453465f6
Anchal-code <79366392+anchal-code@users.noreply.github.com>
2022-03-02 22:15:24 +0530

    kk

--- app/src/main/java/com/example/medicalonlineapp/MainActivity.kt
+++ app/src/main/java/com/example/medicalonlineapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.medicalonlineapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var actionBar = getSupportActionBar()
+
+        if (actionBar != null) {
+
+            // Customize the back button
+            actionBar.setHomeAsUpIndicator(R.drawable.backnavigation);
+
+            // showing the back button in action bar
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
